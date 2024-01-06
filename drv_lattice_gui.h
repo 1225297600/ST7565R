ad2e2c6df2fc3d83bf7c5823bc81a381af6866b3
张治灵 <50475813+1225297600@users.noreply.github.com>
2024-01-06 17:15:19 +0800

    Update drv_lattice_gui.h
    Modify the format to make it more standardized

--- drv_lattice_gui.h
+++ drv_lattice_gui.h
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 
 /*
-	xy默认使用，趋于0的方向
+	xy脛卢脠脧脢鹿脫脙拢卢脟梅脫脷0碌脛路陆脧貌
 */
 
 typedef struct{
@@ -14,8 +14,8 @@ typedef struct{
 	}print;
 	
 	struct{
-		void (*DrawPoint)(uint16_t x, uint16_t y, bool on);
-		void (*DrawUpload)(void);
+		void (*drawPoint)(uint16_t x, uint16_t y, bool on);
+		void (*drawUpload)(void);
 	}hal;
 }DRV_LATTICE_GUI;
 
@@ -24,12 +24,16 @@ typedef enum{
 	GUI_FONTS_W8H16,
 }DRV_LATTICE_GUI_FONTS;
 
+void DRV_LATTICE_GUI_Init(DRV_LATTICE_GUI *gui, 
+		char *buff, uint16_t len,
+		void (*drawPoint)(uint16_t x, uint16_t y, bool on),
+		void (*drawUpload)(void));
 void DRV_LATTICE_GUI_Upload(DRV_LATTICE_GUI *gui);
 
 void DRV_LATTICE_GUI_Point(DRV_LATTICE_GUI *gui, uint16_t x, uint16_t y, bool on);
 void DRV_LATTICE_GUI_Line(DRV_LATTICE_GUI *gui, uint16_t start_x, uint16_t start_y, uint16_t end_x, uint16_t end_y, bool on);
 void DRV_LATTICE_GUI_Fill(DRV_LATTICE_GUI *gui, uint16_t x, uint16_t y, uint16_t w, uint16_t h, bool on);
-//horiz=true 水平扫描，lsb小端模式（低低）
+//horiz=true 脣庐脝陆脡篓脙猫拢卢lsb脨隆露脣脛拢脢陆拢篓碌脥碌脥拢漏
 void DRV_LATTICE_GUI_Imag(DRV_LATTICE_GUI *gui, uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint8_t *img, bool horiz, bool lsb);
 void DRV_LATTICE_GUI_Print(DRV_LATTICE_GUI *gui, DRV_LATTICE_GUI_FONTS fonts, uint16_t x, uint16_t y, char* fmt,...);
 void DRV_LATTICE_GUI_Round(DRV_LATTICE_GUI *gui, uint16_t center_x, uint16_t center_y, uint16_t r, bool on);
